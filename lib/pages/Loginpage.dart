c91aeb9c8ff9f64387204f6c1087c3afd91245cb
BrianKawinzi <brian.mutisya@starthmore.edu>
2023-09-08 14:14:25 +0300

    loginpage.dart

---
+++ lib/pages/Loginpage.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:medfastgo/pages/components/my_button.dart';
+import 'package:medfastgo/pages/components/my_textfield.dart';
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children:  [
+              const SizedBox(height: 50),
+              //logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(height: 50),
+              
+              
+              //welcome back you've been missed
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                  ),
+              ),
+
+              const SizedBox(height: 25),
+              //username textfield
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+              //password textfield
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              
+
+              const SizedBox(height: 10),
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 25),
+              //sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+              
+              const SizedBox(height: 50),
+              
+              //or continue with
+              
+              //google + apple sign in buttons
+              
+              //not a member register now
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

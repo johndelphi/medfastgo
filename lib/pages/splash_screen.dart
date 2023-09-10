24af684b0863ea8ac5edb73b96120a20cf078fbd
BrianKawinzi <brian.mutisya@starthmore.edu>
2023-09-10 16:11:07 +0300

    laoding logic

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
 
-
 class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+
     return Scaffold(
-        backgroundColor: Colors.green,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                '/Users/johnmichael/medfastgo/lib/assets/Medrxlogo.png', // Replace with your image file path
-              ),
-              const SizedBox(height: 20), // Optional: Add some spacing between the logo and text
-              const Text(
-                'MedFast',
-                style: TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ],
-          ),
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              '/Users/johnmichael/medfastgo/lib/assets/Medrxlogo.png',
+              // Replace with your image file path
+            ),
+            const SizedBox(height: 20), // Optional: Add some spacing between the logo and text
+            const Text(
+              'MedFast',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
